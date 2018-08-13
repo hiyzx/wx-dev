4d0ee8c70b15e1b343a1f67c28e9462396bf5d47
hiyzx <452002276@qq.com>
2018-08-13 23:16:27 +0800

    新增查看菜单

--- official-accounts/src/main/java/com/zero/official/accounts/service/IWxService.java
+++ official-accounts/src/main/java/com/zero/official/accounts/service/IWxService.java
@@ -11,4 +11,6 @@ public interface IWxService {
     void checkSignature(String signature, String timestamp, String nonce) throws BaseException;
 
     String getAccessToken() throws BaseException;
+
+    String getMenu() throws BaseException;
 }
---
+++ official-accounts/src/main/java/com/zero/official/accounts/web/controller/WxManagerController.java
@@ -0,0 +1,27 @@
+package com.zero.official.accounts.web.controller;
+
+import com.zero.official.accounts.service.IWxService;
+import com.zero.official.accounts.web.exception.BaseException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/manager")
+public class WxManagerController {
+
+    @Resource
+    private IWxService wxService;
+
+    @GetMapping("/getAccessToken")
+    public String getAccessToken() throws BaseException{
+        return wxService.getAccessToken();
+    };
+
+    @GetMapping("/getMenu")
+    public String getMenu() throws BaseException{
+        return wxService.getMenu();
+    }
+}
